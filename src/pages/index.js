df66a4bb375139775871e3725b67e4654c558dd7
gajender bhadana <monikachandilagurjar#gmail.com>
2022-01-11 17:07:00 +0530

    initial commit

---
+++ src/pages/index.js
@@ -0,0 +1,21 @@
+import Home from "./HomePage";
+import About from "./AboutPage";
+import Error from "./ErrorPage";
+import Product from "./ProductsPage";
+import SingleP from "./SingleProductPage";
+import Cart from "./CartPage";
+import Checkout from "./CheckoutPage";
+import PrivateRoute from "./PrivateRoute";
+import AuthWrapper from "./AuthWrapper";
+
+export {
+  Home,
+  About,
+  Error,
+  Product,
+  SingleP,
+  Cart,
+  Checkout,
+  PrivateRoute,
+  AuthWrapper,
+};
